d1b0d235c45cfca8ec1324ee6c2743e081f7877a
szheron <heronbr7@gmail.com>
2024-02-29 14:47:27 -0300

    feat: added more style

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -31,7 +31,7 @@ export default function Dropdown(props: IDropdown){
                     props.items.map((item,index) =>
                         <button
                             key={index}
-                            className={`${item.selected&&"bg-zinc-600/30 text-white rounded-full"} self-start text-zinc-500 hover:bg-zinc-700 px-4 py-2`}
+                            className={`${item.selected&&"bg-zinc-600/30 text-white rounded-full"} self-start text-zinc-500 hover:bg-zinc-700 hover:rounded-full px-4 py-2`}
                         >
                             {item.text}
                         </button>
--- components/Progressbar.tsx
+++ components/Progressbar.tsx
@@ -7,13 +7,13 @@ export default function Progressbar(props: IProgressbar){
     const progress = Math.floor((props.value / props.total) * 100)
 
     return (
-        <div className="my-4">
+        <div className="my-2">
             <div className="flex justify-between mb-1">
                 <span className="font-medium text-zinc-500 text-sm">Progresso</span>
                 <span className="text-sm font-semibold dark:text-white">{props.value}/{props.total}</span>
             </div>
-                <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-                <div className={"bg-blue-600 h-2.5 rounded-full"} style={{width: progress}}></div>
+                <div className="w-full rounded-full h-2 bg-gray-700">
+                <div className={`bg-green-600 h-2 rounded-full`} style={{width: `${progress}%`}}></div>
             </div>
         </div>
     )
