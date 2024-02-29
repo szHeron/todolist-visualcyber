99d8b072b736519598339aa5683f14a630e0c819
szheron <heronbr7@gmail.com>
2024-02-29 09:10:17 -0300

    feat: added navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Board from "@/components/Board";
 import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
 
 interface ITasks {
   title: string,
@@ -18,9 +19,12 @@ export interface ITodo {
 
 export default function Home() {
   return (
-    <main className="flex flex-col h-screen bg-zinc-800">
-      <Header/>
-      <Board/>
+    <main className="flex flex-row h-screen bg-zinc-800 bg">
+      <Navbar/>
+      <div className="flex flex-col h-full w-full">
+        <Header/>
+        <Board/>
+      </div>
     </main>
   );
 }
